00a2db52beaf4891062517ade4f3188f0c91a9ff
Ronaldo <ronaldcollo172@gmail.com>
2024-10-14 18:59:01 -0500

    Sisben practice

---
+++ database/migrations/2024_10_14_235209_create_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice', 64)->unique();
+            $table->text('product_id')->nullable();
+            $table->text('quantity')->nullable();
+            $table->text('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+};
